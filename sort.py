89f9ddfe9eed8436ddad75319aa70e59c818e636
Kashif Bandali <93444111+kashband@users.noreply.github.com>
2023-06-17 16:42:57 -0500

    added sort file

---
+++ sort.py
@@ -0,0 +1,9 @@
+# sort.py
+# Authored by Kashif Bandali 6/17/23
+# Purpose: 
+
+def sorting_time():
+    pass
+
+if __name__ == "__main__": # when you run the program --> runs this statement
+    sorting_time()
