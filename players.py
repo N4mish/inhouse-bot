cce752b589c1da01bce2c4ebd6ecf66165e773dd
Kashif Bandali <93444111+kashband@users.noreply.github.com>
2023-06-18 14:20:11 -0500

    dummy players created, mmr function (beta) done

--- players.py
+++ players.py
@@ -3,10 +3,18 @@
 
 
 class Player:
-    role_prefs = {}
-    def __init__(self, rank, role_prefs):
+    role_prefs = {} # Empty list of role prefs, will be populated when player constructed
+
+    # Constructor to create the player
+    def __init__(self, ign: str, rank: str, role_prefs: list[str]) -> None:
+        self.ign = ign
         self.rank = rank
         self.role_prefs = role_prefs
     
-    def get_prefs(self):
+    # Function that returns the role preferences of a specific plaer
+    def get_prefs(self) -> list[str]:
         return self.role_prefs
+    
+    # Function that sets the mmr of a specific player
+    def set_mmr(self, mmr: int) -> None:
+        self.mmr = mmr
--- sort.py
+++ sort.py
@@ -2,25 +2,41 @@
 # Authored by Kashif Bandali 6/17/23
 # Purpose: 
 
-import players
+# Import statements
+from players import Player
+import random
 
-player_rating = {}
-player_pref = {}
-role_pref = {}
+# Data structures
+ranks = {"iron": 1, "bronze": 2, "silver": 3, "gold": 5, "platinum": 7, "diamond": 9, "master": 11, "grandmaster": 13, "challenger": 15}
 
-player_pref["Teresa"] = ["TOP", "JNG", "MID", "ADC", "SUP"]
-player_pref["Kashif"] = ["MID", "ADC", "JNG", "SUP", "TOP"]
-player_pref["Namish"] = ["SUP", "JNG", "MID", "ADC", "TOP"]
-player_pref["Sahand"] = ["JNG", "SUP", "MID", "ADC", "TOP"]
-player_pref["Obama"] = ["MID", "JNG", "TOP", "SUP", "ADC"]
+# Create a bunch of players
+kashif = Player("ogopa", "diamond", ["MID", "SUP", "JNG", "ADC", "TOP"])
+teresa = Player("bubbiextea", "silver", ["MID", "SUP", "ADC", "TOP", "JNG"])
+namish = Player("a snowy night", "platinum", ["JNG", "SUP", "TOP", "MID", "ADC"])
+sahand = Player("playernumber6", "master", ["TOP", "SUP", "ADC", "MID", "JNG"])
+obama = Player("baracktheg0at", "iron", ["MID", "ADC", "JNG", "SUP", "TOP"])
+players = [kashif, teresa, namish, sahand, obama]
 
+# Function that calcultes each player's "mmr"
+# Inputs: A player
+# Outputs: None (sets the player's mmr internally)
+def calculate_mmr(player: Player) -> None:
+    base = random.randint(1, 10) # Element of randomness
+    player.set_mmr(base * ranks[player.rank])
+    
+# Function that returns a dictionary mapping each role to their respective player
+# Inputs: A dictionary (dict{str:list[str]})
+# REMEMBER: OPTIMAL FOR THE PARTY OFFERING THE MATCH
+def gale_shapley(playerPref, rankPref) -> dict:
+    # Iterate through the list of roles...
 
-
-
-
-
-def sorting_time():
     pass
 
 if __name__ == "__main__": # when you run the program --> runs this statement
-    sorting_time()
+    # for player in players:
+    #     calculate_mmr(player)
+    #     print(player.ign + ": " + str(player.rank) + ", " + str(player.mmr))
+    # Some hardcode for now that sets players mmr:
+    for player in players:
+        player.mmr = ranks[player.rank]
+    #sorting_time()
